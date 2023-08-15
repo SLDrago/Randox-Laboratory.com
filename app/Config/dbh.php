bb01624d5f4e37f1f5e48f6c30ac2daae06950c7
Dilshan Oshada <86113915+SLDrago@users.noreply.github.com>
2023-08-15 15:24:16 +0530

    Initial Commit

---
+++ app/Config/dbh.php
@@ -0,0 +1,9 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "randox_laboratory";
+
+$conn = mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
+

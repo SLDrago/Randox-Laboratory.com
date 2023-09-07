4d38a676ac243820ad255fc4a872124f03fb636c
KalanaPiyumantha <137792938+KalanaPiyumantha@users.noreply.github.com>
2023-09-07 11:44:04 +0530

    How to prepare frontend create

---
+++ User_Howtoprepare.php
@@ -0,0 +1,59 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>How to Prepare</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/User_H0wtoprepare.css">
+    <style>
+
+    </style>
+</head>
+<body style="background-color: #a7c8ff;">
+<header>
+    <a href="#" class="brand"> <img src="assets/images/logo/logo.png" alt="Logo" width="250px" height="70px"></a>
+
+    <div class="menu-btn">
+
+    </div>
+    <div class="navigation">
+        <div class="navigation-items">
+
+            <a href="#" class="nav-link"><b>Home</b></a>
+            <a href="#" class="nav-link"><b>How to prepare</b></a>
+            <a href="#" class="nav-link"><b>Appointment</b></a>
+            <a href="#" class="nav-link"><b>Download</b></a>
+            <a href="#" class="nav-link"><b>Contact us</b></a>
+            <a href="#"><button><b>Login</button></b></a>
+
+        </div>
+    </div>
+</header>
+
+<hr>
+<div class="image-section">
+
+
+</div>
+<div class="image-section">
+    <div class="image-description">
+        <h2>Full blood Count</h2>
+        <p> The Full Blood Count (FBC) test provides essential insights into your blood composition, including red blood cells, white blood cells, and platelets. This test aids in diagnosing conditions like anemia and infections, supporting your overall health assessment.</p>
+    </div>
+    <img src="assets/images/image1.jpg" alt="Image Description" class="side-image">
+</div>
+<hr>
+<div class="image-section">
+    <div class="image-description">
+        <h2>Urine Test</h2>
+        <p>Our comprehensive urine tests provide valuable insights into your health by analyzing various components present in your urine. These tests can help detect conditions such as urinary tract infections, kidney problems, and metabolic disorders. Trust our accurate urine test results to contribute to your overall well-being.</p>
+    </div>
+    <img src="assets/images/image2.jpg" alt="Image Description" class="side-image">
+</div>
+
+
+</body>
+</html>
+

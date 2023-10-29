3619700b20620154f4e8b23659171ba3ab54a870
ThiliPathum <142502768+ThiliPathum@users.noreply.github.com>
2023-10-29 14:08:02 +0530

    Receptionist Frontend
    I already created frontend of the receptionist.

---
+++ receptionist_Appointment.php
@@ -0,0 +1,197 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>APPOINTMENTS</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="assets/css/receptionist_Appointment_style.css" type="text/css" rel="stylesheet">
+
+</head>
+
+<body>
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+
+
+<div class="container-fluid">
+    <div>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <img class="navbar-logo" src="assets/images/logo/logo.png" width="350px" style="border: 2px solid black;border-radius: 10px">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav" class="nav1">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="Receptionist_Dashboard.php"><b>HOME</b></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="receptionist_Appointment.php"><b>APPOINTMENTS</b></a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                           aria-expanded="false">
+                            <b>TESTS</b>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="receptionist_test.php"><b>PRICING</b></a></li>
+                            <li>
+                            <li><a class="dropdown-item" href="receptionist_Prepare.php"><b>HOW TO PREPARE</b></a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="receptionist_Login.php"><b>REPORTS</b></a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="receptionist_Payment.php"><b>PAYMENTS</b></a>
+                    </li>
+
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                           aria-expanded="false">
+                            <b>LOGIN</b>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#"><b>PROFILE</b></a></li>
+                            <li>
+                            <li><a class="dropdown-item" href="#"><b>ADMINISTRATION</b></a></li>
+                            <li>
+                            <li><a class="dropdown-item" href="#"><b>LOG OUT</b></a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                        </ul>
+                    </li>
+
+                    <!--      <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>-->
+
+                </ul>
+            </div>
+        </nav>
+    </div>
+</div>
+
+<body>
+<br><br>
+
+<div class="container">
+    <div class="appointments">
+        <h2 class="header">Appointments</h2>
+        <br>
+
+        <button class="btn btn-outline-success" type="submit"
+                style="float: right; padding-left: 5px;">Filter</button>
+
+        <div class="form-group" style="float: right;margin-right:5px;border: 1px solid black;">
+
+            <select id="AppointmentMethod">
+                <option value="">Appointments</option>
+                <option value="tommorow">Tommorrow</option>
+                <option value="today">Today</option>
+                <option value="yesterday">Yesterday</option>
+            </select>
+        </div>
+
+
+
+
+        <br><br><br>
+
+        <div class="appointment-list">
+
+
+            <table>
+                <thead>
+                <tr>
+                    <th>Date & Time</th>
+                    <th>Patient Name</th>
+                    <th>Appointment Type</th>
+                    <th>Appointment ID</th>
+                    <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>2023-10-25 10:00 AM</td>
+                    <td>Kamal Liyanage</td>
+                    <td>Online</td>
+                    <td>001</td>
+                    <td>
+                        <button class="btn btn-success" style="margin-right: 10px;">EDIT</button>
+                        <button class="btn btn-danger">REMOVE</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>2023-10-25 11:30 AM</td>
+                    <td>Sanath Jayasuriya</td>
+                    <td>On Walk</td>
+                    <td>002</td>
+                    <td>
+                        <button class="btn btn-success" style="margin-right: 10px;">EDIT</button>
+                        <button class="btn btn-danger">REMOVE</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>2023-10-25 13:30 AM</td>
+                    <td>Amal Shantha</td>
+                    <td>On Walk</td>
+                    <td>003</td>
+                    <td> <button class="btn btn-success" style="margin-right: 10px;">EDIT</button>
+                        <button class="btn btn-danger">REMOVE</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>2023-10-25 14:30 AM</td>
+                    <td>Kamal Shantha</td>
+                    <td>On Call</td>
+                    <td>004</td>
+                    <td> <button class="btn btn-success" style="margin-right: 10px;">EDIT</button>
+                        <button class="btn btn-danger">REMOVE</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>2023-10-25 15:30 AM</td>
+                    <td>Angelo Mathews</td>
+                    <td>On Walk</td>
+                    <td>005</td>
+                    <td> <button class="btn btn-success" style="margin-right: 10px;">EDIT</button>
+                        <button class="btn btn-danger">REMOVE</button>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <!-- <div class="today-appointments">
+        <h3>Today's Appointments</h3>
+        <ul>
+            <li>Patient 1 - 10:00 AM (Online)</li>
+            <li>Patient 2 - 11:30 AM (On Walk)</li>
+
+        </ul>
+    </div>-->
+    </div>
+</div>
+</body>
+
+</html>
---
+++ receptionist_Prepare.php
@@ -0,0 +1,216 @@
+<?php
+?>
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PRICING</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="assets/css/receptionist_Prepare_style.css" type="text/css" rel="stylesheet">
+
+</head>
+
+<body>
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+
+
+<div class="container-fluid">
+    <div>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <img class="navbar-logo" src="assets/images/logo/logo.png" width="350px" style="border: 2px solid black;border-radius: 10px">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav" class="nav1">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="Receptionist_Dashboard.php"><b>HOME</b></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="receptionist_Appointment.php"><b>APPOINTMENTS</b></a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                           aria-expanded="false">
+                            <b>TESTS</b>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="receptionist_test.php"><b>PRICING</b></a></li>
+                            <li>
+                            <li><a class="dropdown-item" href="receptionist_Prepare.php"><b>HOW TO PREPARE</b></a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="receptionist_Login.php"><b>REPORTS</b></a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="receptionist_Payment.php"><b>PAYMENTS</b></a>
+                    </li>
+
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                           aria-expanded="false">
+                            <b>LOGIN</b>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#"><b>PROFILE</b></a></li>
+                            <li>
+                            <li><a class="dropdown-item" href="#"><b>ADMINISTRATION</b></a></li>
+                            <li>
+                            <li><a class="dropdown-item" href="#"><b>LOG OUT</b></a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                        </ul>
+                    </li>
+
+                    <!--      <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>-->
+
+                </ul>
+            </div>
+        </nav>
+    </div>
+</div>
+
+
+<div class="container">
+    <h1 class="text-center mt-4">Test Preparation Instructions</h1>
+    <br>
+    <div class="instruction">
+        <h2 class="header">Full Blood Count</h2>
+        <br><br>
+        <p>
+        <h3 style="color:rgb(35, 125, 163)">Indication</h3>
+        <p> Helps determines the risk of Human Body.Evaluates fat metabolism Helps confirm diagnosis of nephrotic
+            syndrome Pancreatitis.</p>
+        <br>
+
+        <h3 style="color:rgb(35, 125, 163)">Description</h3>
+        <p> The full blood count also called the complete blood count is a test which is routinely done on all
+            patients
+            complaining of an ailment. Because this test can give an idea about your overall health, as
+            it is a measure of many different components in your blood, which is all the different types of cells
+            present in the blood.</p>
+        <br>
+
+        <h3 style="color:rgb(35, 125, 163)"> Patient's Preparation</h3>
+
+        <ul>
+
+            <li>There are no special preparations that you have to make before you undergo this test.</li>
+        </ul>
+        </p>
+
+        <br>
+    </div>
+    <div class="instruction">
+        <h2 class="header">Lipid Profile Test</h2>
+        <br><br>
+        <p>
+        <h3 style="color:rgb(35, 125, 163)">Indication</h3>
+        <p>Lipid disorders. Helps determines the risk of heart disease, especially the risk of coronary- artery
+            disease. Evaluates fat metabolism Helps confirm diagnosis of nephrotic syndrome Pancreatitis, liver
+            disease, hypothyroidism (CHD)</p>
+        <br>
+
+        <h3 style="color:rgb(35, 125, 163)">What "high" or "increased" may indicate</h3>
+        <ul>
+            <li>Hyperthyroidism</li>
+            <li>Early hepatitis</li>
+            <li>Liver disease</li>
+            <li>Consumption of a high -fat diet</li>
+            <li>Hypothyroidism Increase risk of coronary- artery disease</li>
+            <li>Pancreafitis</li>
+            <li>Liver disease</li>
+            <li>Hypothyroidism</li>
+            <li>Malnutrition</li>
+
+
+        </ul>
+        <br>
+
+        <h3 style="color:rgb(35, 125, 163)"> Patient's Preparation</h3>
+
+        <ul>
+
+            <li>Overnight fasting is required. Minimum 12 hours. (Fasting is mandatory)</li>
+            <li>Adults: 12 -14 hrs</li>
+            <li>Pediatric less than 6 yrs: Just before the feed/meal</li>
+            <li>Pediatric older than 6 yrs: Overnight fast specially the fasting period</li>
+            <li>For optimal results, the patient should be on a stable diet for 2- 3 weeks prior to testing. Fasting
+                is
+                defined as no consumption of food or beverages other than water before blood collection.</li>
+        </ul>
+        </p>
+
+        <br>
+    </div>
+
+    <div class="instruction">
+        <h2 class="header">Urine Analysis</h2>
+        <br><br>
+        <p>
+        <h3 style="color:rgb(35, 125, 163)">Indication</h3>
+        <p>This test along with Homo vanillic acid is useful for screening children for catecholamine Secreting
+            tumor Helps
+            detect pheochromocytoma tumor and some disorders of the autonomic nervous system. Evaluates the function
+            of adrenal medulla</p>
+        <br>
+
+        <h3 style="color:rgb(35, 125, 163)">What "high" or "increased" may indicate</h3>
+        <ul>
+            <li>Catecholamine secreting tumor such as pheochromocytoma.</li>
+            <li>Endocrine disorders</li>
+            <li>Liver disease</li>
+
+
+        </ul>
+        <br>
+
+        <h3 style="color:rgb(35, 125, 163)"> Patient's Preparation</h3>
+
+        <ul>
+
+            <li>This patient should strictly avoid Theophylline, Chocolate, Vanilla, Banana, Alcoholic beverages,
+                Tea/Coffee, Tobacco,
+                and strenuous exercise at least 72 hrs before and during collection.</li>
+
+            <li>The following drugs may affect test results, you may be asked not to take this medications before
+                the test.</li>
+            <li>Amino salicylic acid (PAS)</li>
+            <li>Penicillin</li>
+            <li>MAO Inhibitors Reserpine</li>
+            <li>Methocarbamol</li>
+            <li>Clofibrate</li>
+            <li>Aspirin</li>
+            <li>Phenazopyridine
+            <li>Mephenesin</li>
+        </ul>
+        </p>
+
+        <br>
+
+    </div>
+    <!-- Add more instructions here -->
+</div>

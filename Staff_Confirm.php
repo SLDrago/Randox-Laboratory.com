8afbee52dddc5b3935582ca9a7e52ee38b23b718
ThiliPathum <142502768+ThiliPathum@users.noreply.github.com>
2023-11-20 23:38:39 +0530

    I created customer class and user_confirm/user forget/user login/password_change class

---
+++ Staff_Confirm.php
@@ -0,0 +1,68 @@
+<?php
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Randox-Laboratory | Code Verification </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="assets/css/receptionist_Login_style.css" type="text/css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
+
+<div class="container">
+    <div class="left">
+        <img src="assets/images/Login/Enter%20OTP.png"
+             alt="Receptionist Image" width="500px">
+    </div>
+
+    <div class="col-md-6">
+        <div class="login-box">
+
+
+            <br>
+            <form action="" method="post">
+
+                    <h2 class="header">Check your Mobile Phone</h2>
+
+                    <br>
+                    <form action="" method="post">
+                        <div class="mb-3">
+                            <label for="phone" class="form-label"><i class="fas fa-phone"></i> Phone Number: </label>
+                            <input type="text" id="phone" name="phone" class="form-control"
+                                   placeholder="Enter your phone number" required>
+                        </div>
+
+                        <br>
+                        <div class="d-grid">
+                            <button type="submit" class="btn btn-primary" name="login">CONFIRM</button>
+                        </div>
+                    </form>
+
+                </div>
+            </form>
+
+        </div>
+    </div>
+
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+</body>
+
+</html>
+
+
